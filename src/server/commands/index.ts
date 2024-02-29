a46964e632732aa302570d13589759ca62c39eb8
Sharafudheen KK <sharafudheen@ispgweb.com>
2024-02-29 14:48:55 +0530

    edc metrics removed

--- src/server/commands/index.ts
+++ src/server/commands/index.ts
@@ -1,12 +1,10 @@
 import { LoadAppSettingsCommand } from './app-settings.command';
 import { ClearMetricsCommand } from './clear-metrics.command';
-import { LoadEdcMetricsCommand } from './load-edc-metrics.command';
 import { CreateSuperUserCommand } from './superuser.command';
 import { TestSendMailCommand } from './test-send-mail.command';
 
 export const commands = [
   LoadAppSettingsCommand,
-  LoadEdcMetricsCommand,
   CreateSuperUserCommand,
   ClearMetricsCommand,
   TestSendMailCommand,
